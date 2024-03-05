638951424792083e32ef79e84b10114157bb6bbf
Anurag Anand <Anurag Anand@TYSS-Anurag-Anand>
2024-03-05 13:19:24 +0530

    Hello World

---
+++ Shoppersstack/src/main/java/com/shoppersstack/testscripts/NavigateToWelcomePage.java
@@ -0,0 +1,34 @@
+package com.shoppersstack.testscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NavigateToWelcomePage {
+	WebDriver driver;
+
+	@Test(priority = 1)
+	public void openBrowser() {
+		driver = WebDriverManager.firefoxdriver().create();
+		driver.manage().window().maximize();
+	}
+
+	@Test(priority = 2)
+	public void navigateToUrl() {
+		driver.navigate().to("https://www.shoppersstack.com/");
+	}
+
+	@Test(priority = 3)
+	public void verifyPageTitle() {
+		String expectedtitle = "ShoppersStack";
+		String actualTitle = driver.getTitle();
+		if (actualTitle.equals(expectedtitle)) {
+			Reporter.log(actualTitle);
+
+		}
+
+	}
+
+}
